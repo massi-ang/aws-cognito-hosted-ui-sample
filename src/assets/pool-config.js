ef9dacc15b22a87020c27e0a7d64f9bfe1084f1a
Massimiliano Angelino <angmas@amazon.com>
2020-07-24 13:45:18 +0200

    empty config

---
+++ src/assets/pool-config.js
@@ -0,0 +1,9 @@
+const pool = {
+    USER_POOLID: '',
+    CLIENTID: '',
+    HOSTED_UI: 'https://<domain>.<region>.amazoncognito.com',
+    REGION: 'us-east-1',
+    HTTPS_REDIRECT_GW_URI: ''
+}
+
+module.exports = pool;
